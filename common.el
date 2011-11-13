d02955aeeae6df55da718b59715dbd5f811688a1
Miguel Guedes <guedesm@abstratti.com>
2011-11-13 12:09:12 +0000

    * Set default font size back to 9.

--- common.el
+++ common.el
@@ -148,7 +148,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:stipple nil :background "#1a0f0b" :foreground "#c3be98" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 107 :width normal :foundry "unknown" :family "Liberation Mono"))))
+ '(default ((t (:stipple nil :background "#1a0f0b" :foreground "#c3be98" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 90 :width normal :foundry "unknown" :family "Liberation Mono"))))
  '(Info-title-1-face ((t (:weight bold))) t)
  '(font-lock-comment-face ((t (:foreground "#999d63"))))
  '(linum ((t (:foreground "#696969")))))

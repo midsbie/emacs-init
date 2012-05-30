7dde5cba7e5f8eed4e66c266b88f776c63b502e5
Miguel Guedes <guedesm@abstratti.com>
2012-05-30 08:41:42 +0100

    * Added newsgroups settings. * Added imap settings. * Other settings added.

--- common.el
+++ common.el
@@ -144,8 +144,25 @@
 (split-window-horizontally)               ; two windows at startup
 
 ;; Setup NNTP newsgroups
-(setq gnus-select-method '(nntp "eunews.blocknews.net"))
-(setq gnus-read-active-file nil)
+(setq gnus-select-method '(nntp "eunews.blocknews.net")
+      user-full-name "Miguel Guedes"
+      user-mail-address "miguel.a.guedes@gmail.com"
+      nntp-authinfo-file "~/.authinfo"
+      gnus-read-active-file nil)
+
+(setq gnus-secondary-select-methods
+      '(nnimap "Personal"
+               (nnimap-address "imap.gmail.com")
+               (nnimap-server-port 993)
+               (nnimap-stream ssl)))
+
+(setq message-send-mail-function 'smtpmail-send-it
+      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+      smtpmail-auth-credentials '(("smtp.gmail.com" 587 "miguel.a.guedes@gmail.com" nil))
+      smtpmail-default-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-server "smtp.gmail.com"
+      smtpmail-smtp-service 587
+      smtpmail-local-domain "abstratti.com")
 
 ;; Let's now initialise this thing!
 (init)

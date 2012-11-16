f2dd606a6500530903cc7e40001d11eb8b6ace32
Miguel Guedes <guedesm@abstratti.com>
2012-11-16 10:23:17 +0000

    * Fixed final issues with X-specific initialization.

--- conf-common.el
+++ conf-common.el
@@ -122,6 +122,7 @@
  standard-indent 2
  tab-width 2)
 
+; set tab-stop positions for C-i
 (setq tab-stop-list (quote (2 4 6 8 10 12 14 16 18 20 22 24 26 28 30 32 34 36 38 40 42 44 46 48 50 52 54 56 58 60 62 64 66 68 70 72 74 76 78 80 82 84 86 88 90 92 94 96 98 100)))
 (split-window-horizontally)             ; two windows at startup
 
--- conf-x.el
+++ conf-x.el
@@ -1,6 +1,10 @@
 (when (eq window-system 'x)
   (setq frame-title-format "%b - emacs")
   (tool-bar-mode -1)                      ; disable toolbar
+
+  ;; default face
+  (custom-set-faces
+  '(default ((t (:inherit nil :stipple nil :background "#080808" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 88 :width normal :foundry "unknown" :family "Liberation Mono")))))
   
   ;; Let's now perform last initialization steps
   (add-to-list 'default-frame-alist (cons 'width 185))
@@ -15,9 +19,7 @@
   ;; Enable clipboard functionality
   (setq x-select-enable-clipboard t
         interprogram-paste-function 'x-cut-buffer-or-selection-value)
-
-  ;; default face
-  (custom-set-faces
-  '(default ((t (:inherit nil :stipple nil :background "#080808" :foreground "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 88 :width normal :foundry "unknown" :family "Liberation Mono")))))
+	
+  (split-window-horizontally)             ; two windows at startup
   )
 

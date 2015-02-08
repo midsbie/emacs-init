966e885aea905a8f475a5349125edc9cc55d39b3
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-02-08 08:14:46 +0000

    Use improved IDO interface

--- src/init.el
+++ src/init.el
@@ -206,7 +206,15 @@
                                         ; selected text
 
 (setq uniquify-buffer-name-style 'forward)
-(ido-mode t)
+
+;; ido-mode related
+(require 'flx-ido)
+(ido-mode 1)
+(ido-everywhere 1)
+(flx-ido-mode 1)
+;; disable ido faces to see flx highlights.
+(setq ido-enable-flex-matching  t
+      ido-use-faces             nil)
 
 ;; enable useful commands
 (put 'narrow-to-region  'disabled nil)

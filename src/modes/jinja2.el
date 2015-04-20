80b0d15488b0546ec7e07b5d6a333a7ad54ac085
Miguel Guedes <miguel.a.guedes@gmail.com>
2015-04-20 10:33:33 +0100

    Support for jinja2 mode

---
+++ src/modes/jinja2.el
@@ -0,0 +1,35 @@
+;;; jinja2.el --- Configures `jinja2-mode'
+
+;; Copyright (C) 2015  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(add-hook 'jinja2-mode-hook  'init-jinja2)
+
+(defun init-jinja2 ()
+  "Initialise the jinja2 mode."
+  )
+
+(add-to-list 'auto-mode-alist '("\\.j2?\\'" . jinja2-mode))
+
+;;; jinja2.el ends here

27ebe649c38e341503d42be9ef5efa57130c04d4
Miguel Guedes <miguel.a.guedes@gmail.com>
2016-01-27 07:25:23 +0000

    Support ReactJs source files

--- src/modes/js2.el
+++ src/modes/js2.el
@@ -27,6 +27,9 @@
 (require 'js2-mode)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.jsx\\'" . js2-jsx-mode))
+(add-to-list 'auto-mode-alist '("\\.react.js\\'" . js2-jsx-mode))
+
 (setq-default js2-basic-offset  2)
 
 (add-hook 'js2-mode-hook  'init-common-programming)

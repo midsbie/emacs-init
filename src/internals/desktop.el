0cae302d23df6d1286b8bcf600cca941f03b1c90
Miguel Guedes <miguel.a.guedes@gmail.com>
2017-04-26 11:56:16 +0100

    internals: add settings for desktop.el
    Make it so `desktop-change-dir' and `desktop-save' prompt for the current
    working directory by default.

---
+++ src/internals/desktop.el
@@ -0,0 +1,32 @@
+;;; desktop.el --- Configures desktop.el's related settings
+
+;; Copyright (C) 2017  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+;; Make it so `desktop-change-dir' and `desktop-save' prompt for the current
+;; working directory by default.
+(eval-after-load 'desktop-change-dir
+  '(add-to-list 'desktop-path "."))
+
+;;; desktop.el ends here

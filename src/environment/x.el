9116605f5b816e0746d71a4812e461f1a66052d0
Miguel Guedes <miguel.a.guedes@gmail.com>
2017-05-13 09:12:06 +0100

    Slight comment rework

--- src/environment/x.el
+++ src/environment/x.el
@@ -60,10 +60,6 @@ characters wide."
 
   ;; custom faces
   (custom-set-faces
-   ;; custom-set-faces was added by Custom.
-   ;; If you edit it by hand, you could mess it up, so be careful.
-   ;; Your init file should contain only one such instance.
-   ;; If there is more than one, they won't work right.
    '(default ((t (:inherit nil :stipple nil :background "#080808" :foreground
                            "#dcdccc" :inverse-video nil :box nil :strike-through nil :overline nil
                            :underline nil :slant normal :weight normal :height 84 :width normal :foundry
@@ -93,7 +89,7 @@ characters wide."
 
   (tooltip-mode -1)                       ; disable tooltips
 
-  ;; Disable mouse on the 'nyx' laptop.
+  ;; Completely disable mouse on the 'nyx' laptop.
   (cond ((string= (system-name) "nyx")
          (global-disable-mouse-mode 1)))
   )

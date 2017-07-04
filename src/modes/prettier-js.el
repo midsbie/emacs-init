4ba9d30ebe7842304d6f54cd38f0c46a238a5c46
Miguel Guedes <miguel.a.guedes@gmail.com>
2017-07-04 10:37:01 +0100

    modes: add support for prettier-js

---
+++ src/modes/prettier-js.el
@@ -0,0 +1,37 @@
+;;; prettier-js.el --- Configures the prettier-js minor mode
+
+;; Copyright (C) 2017  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+
+(add-hook 'js2-mode-hook 'prettier-js-mode)
+(add-hook 'web-mode-hook 'prettier-js-mode)
+
+;; Default parameters to pass to prettier.
+(setq-default prettier-js-args
+              '("--single-quote"
+                "--trailing-comma" "es5"
+                "--jsx-bracket-same-line"))
+
+;;; pretiter-js.el ends here

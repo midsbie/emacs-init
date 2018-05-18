7063cd34835a94504f3d1923b02699a07a112639
Miguel Guedes <miguel.a.guedes@gmail.com>
2018-05-18 07:06:33 +0100

    extensions: add support for large files

---
+++ src/extensions/large_files.el
@@ -0,0 +1,37 @@
+;;; large_files.el --- Support for editing of large files
+
+;; Copyright (C) 2015-2018  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel@miguelguedes.org>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun init/handle-large-file-hook ()
+  "If a file is over a given size, make the buffer read only and forcefully
+switch to fundamental mode."
+  (when (> (buffer-size) (* 1024 1024))
+    (setq buffer-read-only t)
+    (buffer-disable-undo)
+    (fundamental-mode)))
+
+(add-hook 'find-file-hook 'init/handle-large-file-hook)
+
+;;; large_files.el ends here

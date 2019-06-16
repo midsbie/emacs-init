3018998af3d1a8fc37bac046e0b4dd1e92d747e3
Miguel Guedes <miguel@softgeist.com>
2019-06-16 17:35:00 +0100

    Add configuration for avy and ace-window packages

---
+++ src/features/avy.el
@@ -0,0 +1,43 @@
+;;; avy.el --- Customises ANSI color palette
+
+;; Copyright (C) 2019  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun init/avy ()
+  "Lazily initialise the `avy' package."
+  (avy-setup-default)
+
+  (global-set-key (kbd "C-:") 'avy-goto-char)
+  (global-set-key (kbd "C-'") 'avy-goto-char-2)
+  (global-set-key (kbd "M-g f") 'avy-goto-line)
+  (global-set-key (kbd "M-g w") 'avy-goto-word-1)
+  (global-set-key (kbd "C-c C-j") 'avy-resume)
+)
+
+; Moved to after the function declaration so as to avoid potential failures at
+; load time
+(eval-after-load 'avy '(init/avy))
+
+
+;;; avy.el ends here

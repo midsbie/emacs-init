0e274a4f77b85c085a5d23da85e07b542133a7be
Miguel Guedes <miguel@softgeist.com>
2021-10-16 10:04:05 +0100

    modeline: extend modes to diminish

--- src/internals/modeline.el
+++ src/internals/modeline.el
@@ -25,8 +25,12 @@
 
 ;;; Code:
 
-(setq my/diminished-modes '(projectile-mode ivy-mode eldoc-mode auto-fill-mode yas-minor-mode
-                                            git-gutter-mode))
+(setq my/diminished-modes '(auto-fill-mode auto-fill-function
+                                            display-fill-column-indicator-mode
+                                            git-gutter-mode
+                                            ivy-mode eldoc-mode
+                                            projectile-mode
+                                            yas-minor-mode ))
 
 (dolist (mode my/diminished-modes)
   (add-hook (add-suffix-to-symbol mode "-hook") `(lambda ()

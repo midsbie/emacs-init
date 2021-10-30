1bdb2b66407ed883c4b5efe40291b13caf15cf39
Miguel Guedes <miguel@softgeist.com>
2021-10-30 16:29:07 +0100

    features: add configuration for which-key package

---
+++ src/features/which-key.el
@@ -0,0 +1,45 @@
+;;; which-key.el --- Configures the which-key package
+
+;; Copyright (C) 2021  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun init/which-key ()
+  "Initialise the `which-key' package."
+  ;; The following mostly from:
+  ;; https://github.com/justbur/emacs-which-key
+  ;;
+  ;; Allow C-h to trigger which-key before it is done automatically
+  (setq which-key-show-early-on-C-h t)
+  ;; make sure which-key doesn't show normally but refreshes quickly after it is
+  ;; triggered.
+  (setq which-key-idle-delay 10000)
+  (setq which-key-idle-secondary-delay 0.05)
+  (which-key-setup-side-window-right)
+  (which-key-mode))
+
+(use-package which-key
+  :init
+  (init/which-key))
+
+;;; which-key.el ends here

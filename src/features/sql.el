e98bc03e32f60a37411b3f8703ebd81f6e9eeb8f
Miguel Guedes <miguel@softgeist.com>
2022-06-06 14:44:43 +0100

    features: add configuration for sql package

---
+++ src/features/sql.el
@@ -0,0 +1,35 @@
+;;; sql.el --- Customises the sql Emacs package
+
+;; Copyright (C) 2022  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun init/sql-interactive-mode/config ()
+  (auto-fill-mode -1)
+  (display-fill-column-indicator-mode -1))
+
+(use-package sql
+  :hook ((sql-interactive-mode . init/sql-interactive-mode/config))
+  )
+
+;;; sql.el ends here

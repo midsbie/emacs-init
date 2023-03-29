e46266085b133e7238fc4da16aebd286f817fdbc
Miguel Guedes <miguel@softgeist.com>
2023-03-29 20:40:42 +0100

    ido: disable when consult in use

--- src/features/ido.el
+++ src/features/ido.el
@@ -1,6 +1,6 @@
 ;;; ido.el --- Configures ido and related packages
 
-;; Copyright (C) 2015-2022  Miguel Guedes
+;; Copyright (C) 2015-2023  Miguel Guedes
 
 ;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
 ;; Keywords: tools
@@ -20,7 +20,11 @@
 
 ;;; Commentary:
 
-;;
+;; Configuration of IDO is disabled because it conflicts with the `consult'
+;; package; note :if clause in use-package declaration.  The conflict occurs
+;; when opening a NEW file whose name partially matches that of an existing
+;; file.  When this happens, it is not possible to hit M-RET to instruct consult
+;; to accept the entered name as-is.
 
 ;;; Code:
 
@@ -31,12 +35,13 @@
   (ido-everywhere 1)
   (flx-ido-mode 1)
 
-  ;; Disable ido faces to see flx highlights.
-  ;; Ref: https://github.com/lewang/flx
+  Disable ido faces to see flx highlights.
+  Ref: https://github.com/lewang/flx
   (setq ido-enable-flex-matching  t
         ido-use-faces             nil))
 
 (use-package flx-ido
+  :if (not (featurep 'consult))
   :after ido
   :config (init/ido))
 

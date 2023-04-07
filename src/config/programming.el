88df7961f61bb9823c46ee7295a45b8186056ce8
Miguel Guedes <miguel@softgeist.com>
2023-04-07 18:46:59 +0100

    config: work-around for broken beginning/end of statement functionality

--- src/config/programming.el
+++ src/config/programming.el
@@ -123,9 +123,8 @@
   (local-set-key (kbd "C-x {") 'backward-list)
   (local-set-key (kbd "C-x }") 'forward-list)
 
-  ;; These currently error out in Javascript and Typescript modes
-  ;;   (local-set-key (kbd "M-a") 'c-beginning-of-statement-1)
-  ;;   (local-set-key (kbd "M-e") 'c-end-of-statement)
+  (local-set-key (kbd "M-a") #'my/beginning-of-statement)
+  (local-set-key (kbd "M-e") #'my/end-of-statement)
 
   ;; Configure compile
   (local-set-key (kbd "C-c C-c") 'compile)
---
+++ src/extra-features/buffer.el
@@ -0,0 +1,57 @@
+;;; buffer.el --- Extra features for buffer manipulation
+
+;; Copyright (C) 2023  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel@softgeist.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun my/beginning-of-statement ()
+  "Safe version of `c-beginning-of-statement-1'.
+
+Attempts to run the original function.  In web-related modes like
+`typescript-ts-mode' errors may occur, in which case it will
+attempt to provide a similar result using alternative methods."
+  (interactive)
+  (condition-case nil
+      (c-beginning-of-statement-1)
+    (error
+     (condition-case nil
+         (c-backward-sexp)
+       (error
+        (backward-sentence))))))
+
+(defun my/end-of-statement ()
+  "Safe version of `c-end-of-statement'.
+
+Attempts to run the original function.  In web-related modes like
+`typescript-ts-mode' errors may occur, in which case it will
+attempt to provide a similar result using alternative methods."
+  (interactive)
+  (condition-case nil
+      (c-end-of-statement)
+    (error
+     (condition-case nil
+         (c-forward-sexp)
+       (error
+        (forward-sentence))))))
+
+;;; buffer.el ends here

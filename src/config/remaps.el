c59bfaa826d0d9b638ec58bf5c6cb6ba0e907541
Miguel Guedes <miguel@softgeist.com>
2023-04-07 14:38:52 +0100

    config: fix using deprecated method for key binding

--- src/config/remaps.el
+++ src/config/remaps.el
@@ -76,9 +76,9 @@
 ;; Replace mark-paragraph
 (global-set-key (kbd "M-h")           'backward-kill-word)
 
-;; Ensure that C-left is bound to M-b and C-right to M-f so that keys work
-;; consistently when subword-mode is active.
-(setf (global-key-binding (kbd "C-<left>")) (kbd "M-b")
-      (global-key-binding (kbd "C-<right>")) (kbd "M-f"))
+;; Ensure that C-left is bound to `backward-word' and `forward-word' so that
+;; keys work consistently when subword-mode is active.
+(global-set-key (kbd "C-<left>") 'backward-word)
+(global-set-key (kbd "C-<right>") 'forward-word)
 
 ;;; remaps.el ends here

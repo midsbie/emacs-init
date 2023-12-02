52ca1ef21d577bd218968d79df7fcff1b03ddfd4
Miguel Guedes <miguel@softgeist.com>
2023-12-02 07:53:29 +0000

    features: add sqlformat package configuration

---
+++ src/features/sqlformat.el
@@ -0,0 +1,36 @@
+;;; sqlformat.el --- Customises the sqlformat package
+
+;; Copyright (C) 2023  Miguel Guedes
+
+;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
+;; Keywords: tools
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;;
+
+;;; Code:
+
+(defun init/config/sqlformat ()
+  "Configure `sqlformat' package."
+  (setq sqlformat-command 'pgformatter
+        sqlformat-args '("-s2" "-g" "-w80"))
+)
+
+(use-package sqlformat
+  :config (init/config/sqlformat))
+
+;;; sqlformat.el ends here

3d2af2b290a6fed92e3194daa740e8ea41fef55c
Miguel Guedes <miguel@softgeist.com>
2025-02-14 12:51:27 +0000

    remaps: refactor func declarations

--- src/config/remaps.el
+++ src/config/remaps.el
@@ -32,53 +32,53 @@
 ;;; Code:
 
 ;; Awesome key bindings for moving around and manipulating windows.
-(global-set-key [M-left]              'windmove-left)
-(global-set-key [M-right]             'windmove-right)
-(global-set-key [M-up]                'windmove-up)
-(global-set-key [M-down]              'windmove-down)
-(global-set-key (kbd "C-x p")         'my/other-previous-window)
+(global-set-key [M-left]              #'windmove-left)
+(global-set-key [M-right]             #'windmove-right)
+(global-set-key [M-up]                #'windmove-up)
+(global-set-key [M-down]              #'windmove-down)
+(global-set-key (kbd "C-x p")         #'my/other-previous-window)
 
 ;; Define miscellaneous shortcuts.
 ; Note that ivy defines some keyboard mappings under F2
 ; F2 F2 originally invoked: `2C-two-columns'
-(global-set-key (kbd "<f2> <f2>")     'shell)
-(global-set-key [f3]                  'my/better-next-error)
-(global-set-key [S-f3]                'my/better-previous-error)
-(global-set-key (kbd "C-x k")         'my/kill-current-buffer)
-(global-set-key (kbd "C-x w")         'my/copy-symbol-to-kill-ring)
-(global-set-key (kbd "C-c y")         'browse-kill-ring)
+(global-set-key (kbd "<f2> <f2>")     #'shell)
+(global-set-key [f3]                  #'my/better-next-error)
+(global-set-key [S-f3]                #'my/better-previous-error)
+(global-set-key (kbd "C-x k")         #'my/kill-current-buffer)
+(global-set-key (kbd "C-x w")         #'my/copy-symbol-to-kill-ring)
 (global-set-key (kbd "C-c w")         #'my/select-current-word)
+(global-set-key (kbd "C-c y")         #'browse-kill-ring)
 
 ;; Buffer-related
 ; Was previously using `truncate-lines` but now using the much more useful
 ; visual lines mode:
-;  (global-set-key (kbd "C-S-w")         'toggle-truncate-lines)
-(global-set-key (kbd "C-S-w")         'visual-line-mode)
-(global-set-key (kbd "M-r")           'revert-buffer)
-(global-set-key (kbd "C-x C-b")       'ibuffer)
+;  (global-set-key (kbd "C-S-w")         #'toggle-truncate-lines)
+(global-set-key (kbd "C-S-w")         #'visual-line-mode)
+(global-set-key (kbd "M-r")           #'revert-buffer)
+(global-set-key (kbd "C-x C-b")       #'ibuffer)
 ; C-c g runs counsel-git-grep but we favour our own version since counsel was
 ; found to hang too frequently and cause memory leaks.
-(global-set-key (kbd "C-c g")         'my/git-grep)
-(global-set-key (kbd "C-c G")         'find-grep)
-(global-set-key (kbd "C-x /")         'bury-buffer)
-(global-set-key (kbd "C-x 4 k")       'my/kill-other-buffer)
-(global-set-key (kbd "C-x 4 /")       'my/bury-other-buffer)
-(global-set-key (kbd "C-x +")         'my/rearrange-desktop)
+(global-set-key (kbd "C-c g")         #'my/git-grep)
+(global-set-key (kbd "C-c G")         #'find-grep)
+(global-set-key (kbd "C-x /")         #'bury-buffer)
+(global-set-key (kbd "C-x 4 k")       #'my/kill-other-buffer)
+(global-set-key (kbd "C-x 4 /")       #'my/bury-other-buffer)
+(global-set-key (kbd "C-x +")         #'my/rearrange-desktop)
 ; Prevents collision with ivy's C-x l
-(global-set-key (kbd "C-x L")         'count-lines-page)
+(global-set-key (kbd "C-x L")         #'count-lines-page)
 
-(global-set-key (kbd "C-c C-t")       'c-toggle-hungry-state)
+(global-set-key (kbd "C-c C-t")       #'c-toggle-hungry-state)
 
 ;; Redefine C-h (help) as C-x h and define backspace as C-h. Note that Emacs
 ;; help feature can still be accessed via F1.
-(global-set-key (kbd "C-h")           'delete-backward-char)
+(global-set-key (kbd "C-h")           #'delete-backward-char)
 
 ;; Replace mark-paragraph
-(global-set-key (kbd "M-h")           'backward-kill-word)
+(global-set-key (kbd "M-h")           #'backward-kill-word)
 
 ;; Ensure that C-left is bound to `backward-word' and `forward-word' so that
 ;; keys work consistently when subword-mode is active.
-(global-set-key (kbd "C-<left>") 'backward-word)
-(global-set-key (kbd "C-<right>") 'forward-word)
+(global-set-key (kbd "C-<left>")      #'backward-word)
+(global-set-key (kbd "C-<right>")     #'forward-word)
 
 ;;; remaps.el ends here

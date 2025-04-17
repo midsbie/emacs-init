ca38fa47f2239606bc1b11b5c1f71c33d6359d90
Miguel Guedes <miguel@softgeist.com>
2025-04-17 16:59:46 +0100

    org-mode: refactor using use-package idioms

--- src/modes/org.el
+++ src/modes/org.el
@@ -1,6 +1,6 @@
 ;;; org.el --- Configuration for the `org' package
 
-;; Copyright (C) 2015-2024  Miguel Guedes
+;; Copyright (C) 2015-2025  Miguel Guedes
 
 ;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
 ;; Keywords: tools
@@ -24,76 +24,62 @@
 
 ;;; Code:
 
-(defun init/org/config ()
-  "Org mode load hook."
-  (setq org-use-fast-todo-selection t
-        org-directory               "~/documents/org"
-        org-default-notes-file      (concat org-directory "/notes.org"))
-
-  (setq org-todo-keyword-faces
-        '(("TODO"  . (:foreground "red" :weight bold))
-          ("NEXT"  . (:foreground "red" :weight bold))
-          ("DONE"  . (:foreground "forest green" :weight bold))
-          ("WAITING"  . (:foreground "orange" :weight bold))
-          ("CANCELLED"  . (:foreground "forest green" :weight bold))
-          ("SOMEDAY"  . (:foreground "orange" :weight bold))
-          ("OPEN"  . (:foreground "red" :weight bold))
-          ("CLOSED"  . (:foreground "forest green" :weight bold))
-          ("ONGOING"  . (:foreground "orange" :weight bold))))
-
-  (setq org-clock-persist 'history)
-  (org-clock-persistence-insinuate)
-
-  (setq org-todo-keywords
-        '((sequence "TODO(t)" "IN-PROGRESS(p!)" "WAIT(w@/!)"
-                    "|"
-                    "DONE(d!)" "CANCELED(c@)")))
-
-  ;; (setq org-todo-keywords
-  ;;       '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!/!)")
-  ;;         (sequence "WAITING(w@/!)" "|" "CANCELLED(c@/!)")
-  ;;         (sequence "SOMEDAY(s!/!)" "|")
-  ;;         (sequence "OPEN(O!)" "|" "CLOSED(C!)")
-  ;;         (sequence "ONGOING(o!/!)" "|")))
-
-  (global-set-key (kbd "C-c a a")   'org-agenda-list)
-  (global-set-key (kbd "C-c a t")   'org-todo-list)
-  (global-set-key (kbd "C-c c")     'org-capture)
-  (global-set-key (kbd "C-x K o")   'my/org-kill-all-buffers)
-
-  (custom-set-variables
-   '(org-agenda-window-setup (quote current-window))))
+(defun my/org-kill-all-buffers ()
+  "Kill all `org-mode'-related buffers."
+  (interactive)
+  (mapc #'kill-buffer
+        (seq-filter (lambda (buf)
+                      (with-current-buffer buf
+                        (memq major-mode
+                              '(org-mode org-agenda-mode calendar-mode))))
+                    (buffer-list)))
+  (message "Killed all Org-mode buffers"))
 
 (defun init/org-mode/enable ()
-  ;; Turn on indent and visual line modes by default
+  "Enhance `org-mode' with visual and behavior tweaks."
   (org-indent-mode 1)
-  (visual-line-mode)
+  (visual-line-mode 1)
   (auto-fill-mode -1)
   (display-fill-column-indicator-mode -1)
-
-  ;; Remaps
-  (local-set-key (kbd "M-h") 'backward-kill-word))
-
-(defun my/org-kill-all-buffers ()
-  "Kill all `org-mode' buffers."
-  (interactive)
-  (mapc 'kill-buffer
-        (remove-if-not #'(lambda(buffer)
-                           (with-current-buffer buffer
-                             (or
-                              (string= major-mode "calendar-mode")
-                              (string= major-mode "org-mode")
-                              (string= major-mode "org-agenda-mode"))))
-                       (buffer-list)))
-  (when (called-interactively-p 'interactive)
-    (message "Killed all org-mode buffers")))
+  (local-set-key (kbd "M-h") #'backward-kill-word))
 
 (use-package org
   :hook (org-mode . init/org-mode/enable)
-  ;; The `ein' package requires org-mode to be available when it loads (or it
-  ;; used to), which in turn requires the `speedbar' package to have been
-  ;; loaded.
   :after speedbar
-  :config (init/org/config))
+  :bind
+  (("C-c a a" . org-agenda-list)
+   ("C-c a t" . org-todo-list)
+   ("C-c c"   . org-capture)
+   ("C-x K o" . my/org-kill-all-buffers))
+  :custom-face
+  (org-todo ((t (:foreground "red" :weight bold))))
+  (org-done ((t (:foreground "forest green" :weight bold))))
+  :custom
+  (org-agenda-window-setup 'current-window)
+  (org-clock-persist 'history)
+  (org-confirm-babel-evaluate nil)
+  (org-directory "~/documents/org")
+  (org-startup-with-inline-images t)
+  (org-use-fast-todo-selection t)
+
+  (org-todo-keywords
+   '((sequence "TODO(t)" "IN-PROGRESS(p!)" "WAIT(w@/!)"
+               "|"
+               "DONE(d!)" "CANCELED(c@)")))
+  (org-todo-keyword-faces
+   '(("TODO" . (:foreground "red" :weight bold))
+     ("NEXT" . (:foreground "red" :weight bold))
+     ("DONE" . (:foreground "forest green" :weight bold))
+     ("WAITING" . (:foreground "orange" :weight bold))
+     ("CANCELLED" . (:foreground "forest green" :weight bold))
+     ("SOMEDAY" . (:foreground "orange" :weight bold))
+     ("OPEN" . (:foreground "red" :weight bold))
+     ("CLOSED" . (:foreground "forest green" :weight bold))
+     ("ONGOING" . (:foreground "orange" :weight bold))))
+  :config
+  (org-clock-persistence-insinuate)
+  (setq org-default-notes-file (concat org-directory "/notes.org"))
+)
+
 
 ;;; org.el ends here

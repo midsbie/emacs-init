21ff2770ed8e96459b54f164193bafe6b3b56d29
Miguel Guedes <miguel@softgeist.com>
2026-03-31 16:09:01 +0100

    features(ido): add missing comment markers

--- src/features/ido.el
+++ src/features/ido.el
@@ -1,6 +1,6 @@
 ;;; ido.el --- Configures ido and related packages
 
-;; Copyright (C) 2015-2024  Miguel Guedes
+;; Copyright (C) 2015-2026  Miguel Guedes
 
 ;; Author: Miguel Guedes <miguel.a.guedes@gmail.com>
 ;; Keywords: tools
@@ -35,8 +35,8 @@
   (ido-everywhere 1)
   (flx-ido-mode 1)
 
-  Disable ido faces to see flx highlights.
-  Ref: https://github.com/lewang/flx
+  ;; Disable ido faces to see flx highlights.
+  ;; Ref: https://github.com/lewang/flx
   (setq ido-enable-flex-matching  t
         ido-use-faces             nil))
 

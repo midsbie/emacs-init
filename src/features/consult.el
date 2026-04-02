b4e280586bde63b541ac2a9b63cce52ab6043607
Miguel Guedes <miguel@softgeist.com>
2026-04-02 20:43:05 +0100

    features(consult): remove redundant register-preview-function

--- src/features/consult.el
+++ src/features/consult.el
@@ -100,15 +100,12 @@
   ;;
   ;; Note that this is now set in the `orderless' configuration file.
 
-  ;; Optionally configure the register formatting. This improves the register
-  ;; preview for `consult-register', `consult-register-load',
-  ;; `consult-register-store' and the Emacs built-ins.
-  (setq register-preview-delay    0.5
-        register-preview-function #'consult-register-format)
-
-  ;; Optionally tweak the register preview window.
-  ;; This adds thin lines, sorting and hides the mode line of the window.
+  ;; Tweak the register preview for `consult-register-load',
+  ;; `consult-register-store' and the built-in commands.  This improves the
+  ;; register formatting, adds thin separator lines, register sorting and hides
+  ;; the window mode line.
   (advice-add #'register-preview :override #'consult-register-window)
+  (setq register-preview-delay 0.5)
 
   ;; Use Consult to select xref locations with preview
   (setq xref-show-xrefs-function #'consult-xref

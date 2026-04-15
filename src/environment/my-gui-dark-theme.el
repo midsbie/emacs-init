b1982b11b08f5db074012ecdeec56e1a6b53dff2
Miguel Guedes <miguel@softgeist.com>
2026-04-15 15:07:39 +0100

    environment: prefer modeline with blue background in the active window

--- src/environment/my-gui-dark-theme.el
+++ src/environment/my-gui-dark-theme.el
@@ -13,39 +13,42 @@
 
 ;;; Code:
 
+(require 'color)
+
 (deftheme my-gui-dark
   "Personal dark GUI theme.")
 
-(custom-theme-set-faces
- 'my-gui-dark
+(let ((ml-active-bg "#1a2d4a"))
+  (custom-theme-set-faces
+   'my-gui-dark
 
- ;; Base
- '(default ((t (:foreground "#ffffff" :background "#080808"))))
- '(widget-field ((t (:foreground "black"))))
- '(highlight ((t (:background "#004225"))))
- '(hl-line ((t (:inherit highlight :background "#000f08"))))
+   ;; Base
+   '(default ((t (:foreground "#ffffff" :background "#080808"))))
+   '(widget-field ((t (:foreground "black"))))
+   '(highlight ((t (:background "#004225"))))
+   '(hl-line ((t (:inherit highlight :background "#000f08"))))
 
- ;; Mode line (original background: "grey22")
- '(mode-line-active ((t (:foreground "#d3d7cf" :background "#433c4e"
-                          :box (:line-width 1 :color "grey32")))))
- '(mode-line-inactive ((t (:inherit mode-line-active
-                            :foreground "grey50" :background "grey10"
-                            :box (:line-width 1 :color "grey14")))))
+   ;; Mode line (original background: "grey22")
+   `(mode-line-active ((t (:foreground "#d3d7cf" :background ,ml-active-bg
+                            :box (:line-width 1 :color ,(color-lighten-name ml-active-bg 15))))))
+   '(mode-line-inactive ((t (:inherit mode-line-active
+                              :foreground "grey50" :background "grey10"
+                              :box (:line-width 1 :color "grey14")))))
 
- ;; Font lock
- '(font-lock-doc-face ((t (:foreground "dark salmon"))))
- '(font-lock-property-use-face ((t (:foreground "cornsilk"))))
- '(font-lock-function-call-face ((t (:inherit nil :foreground "LightBlue"))))
- '(font-lock-number-face ((t (:inherit font-lock-warning-face :weight normal))))
- '(font-lock-operator-face ((t (:inherit font-lock-builtin-face))))
+   ;; Font lock
+   '(font-lock-doc-face ((t (:foreground "dark salmon"))))
+   '(font-lock-property-use-face ((t (:foreground "cornsilk"))))
+   '(font-lock-function-call-face ((t (:inherit nil :foreground "LightBlue"))))
+   '(font-lock-number-face ((t (:inherit font-lock-warning-face :weight normal))))
+   '(font-lock-operator-face ((t (:inherit font-lock-builtin-face))))
 
- ;; LSP
- '(lsp-headerline-breadcrumb-path-face ((t (:inherit font-lock-builtin-face :weight normal))))
+   ;; LSP
+   '(lsp-headerline-breadcrumb-path-face ((t (:inherit font-lock-builtin-face :weight normal))))
 
- ;; Speedbar
- '(speedbar-highlight-face ((t (:foreground "dark blue"))))
+   ;; Speedbar
+   '(speedbar-highlight-face ((t (:foreground "dark blue"))))
 
-)
+   ))
 
 (provide-theme 'my-gui-dark)
 
